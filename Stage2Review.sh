132daf4ad0dfd30ae50d2b9dd52b7557e78bb928
mishradibyesh <dibyeshmishra207@gmail.com>
2021-09-21 02:32:45 +0530

    version 2 :solution of the problem is now modified

--- Stage2Review.sh
+++ Stage2Review.sh
@@ -1,5 +1,5 @@
 function funct_palindrome()
-{
+	{
                 new=0;
 		a=$1;
                         while [ $a -gt 0 ]
@@ -9,38 +9,38 @@ function funct_palindrome()
                                         a=$(( $a / 10 ));
                                 done
                 echo "the palindrome of $n is $new and ";
-count=$(( $count + 1 ));
-n=$new;
-funct_Prime 
-}
+		count=$(( $count + 1 ));
+		n=$new;
+		funct_Prime 
+	}
 read -p "please enter a number here : " n;
 count=0;
 
 function funct_Prime()
-{
-        a=0;
-if [ $n -eq 1 ]
-                then
-                        echo "$n is a prime no";
-                else
-                        for (( i=2; i<n; i++ ))
-                                do
-                                        r=$(( $n % $i ));
-                                                if [ $r -eq 0 ]
-                                                        then
-                                                                a=$(( $a+1 ));
-                                                fi
-                        done
-                        if [ $a -eq 0 ]
-                                then
-                                        echo "$n is a prime no";
-					if [ $count -eq 0 ]
-						then
-							funct_palindrome $n
-                        	        fi
-				else
-                                        echo "$n is not a prime no";
-                        fi
-        fi
-}
+	{
+        	a=0;
+			if [ $n -eq 1 ]
+                		then
+                        		echo "$n is a prime no";
+                		else
+                        		for (( i=2; i<n; i++ ))
+                                		do
+                                        		r=$(( $n % $i ));
+                                                	if [ $r -eq 0 ]
+                                                	        then
+                                                        	        a=$(( $a+1 ));
+                                                	fi
+                        			done
+                        			if [ $a -eq 0 ]
+                                			then
+                                        			echo "$n is a prime no";
+							 	 	if [ $count -eq 0 ]
+										then
+											funct_palindrome $n
+                        	        				fi
+							else
+                                        			echo "$n is not a prime no";
+                        			fi
+       			 fi
+	}
 funct_Prime
